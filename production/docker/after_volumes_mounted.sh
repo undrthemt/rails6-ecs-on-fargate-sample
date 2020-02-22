9556ddb4790fa829e1bb6ed8c0f068c1590b0b08
m-yamashita <undrthemt@gmail.com>
2020-02-23 06:09:13 +0900

    [update] pumaのWEB_CONCURRENCYを指定できるよう変更。pidファイルを消す順序を変更。

--- production/docker/after_volumes_mounted.sh
+++ production/docker/after_volumes_mounted.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # For production.
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
 # pumaのipdとsocketファイル用のディレクトリを作成する
 mkdir -p /app/tmp/sockets && mkdir -p /app/tmp/pids
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /app/tmp/pids/server.pid
 cd /app && rails assets:precompile
