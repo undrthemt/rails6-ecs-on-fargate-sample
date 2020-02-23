5928a7383d4cac0b37e611d25a81adb56b950578
m-yamashita <undrthemt@gmail.com>
2020-02-24 05:51:50 +0900

    [update] production用のDockerのイメージを小さくするよう変更。

--- production/docker/after_volumes_mounted.sh
+++ production/docker/after_volumes_mounted.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 # For production.
 # pumaのipdとsocketファイル用のディレクトリを作成する
 mkdir -p /app/tmp/sockets && mkdir -p /app/tmp/pids
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /app/tmp/pids/server.pid
-cd /app && rails assets:precompile
--- production/docker/entrypoint.sh
+++ production/docker/entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # For production.
 
 set -e
